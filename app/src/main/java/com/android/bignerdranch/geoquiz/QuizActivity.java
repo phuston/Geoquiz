586ad13f6e0d04489eb596d77b7f95193f354704
Patrick Huston <patrickhuston1@gmail.com>
2015-02-02 14:34:07 -0500

    Updates questions, increases functionality of cheat feature.

--- app/src/main/java/com/android/bignerdranch/geoquiz/QuizActivity.java
+++ app/src/main/java/com/android/bignerdranch/geoquiz/QuizActivity.java
@@ -26,11 +26,11 @@ public class QuizActivity extends Activity {
     private TextView mQuestionTextView;
 
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
-            new TrueFalse(R.string.question_oceans, true),
-            new TrueFalse(R.string.question_mideast, false),
-            new TrueFalse(R.string.question_africa, false),
-            new TrueFalse(R.string.question_americas, true),
-            new TrueFalse(R.string.question_asia, true),
+            new TrueFalse(R.string.question_erection, true),
+            new TrueFalse(R.string.question_true, false),
+            new TrueFalse(R.string.question_keenan, true),
+            new TrueFalse(R.string.question_byron, false),
+            new TrueFalse(R.string.question_kai, true),
     };
 
     private int mCurrentIndex = 0;
